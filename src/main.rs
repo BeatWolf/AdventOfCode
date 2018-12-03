56d3d397f6e9df94db6b056d081ccaa28083207c
Beat Wolf <beat.wolf@hefr.ch>
2018-12-03 11:48:09 +0100

    Quick solution for day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+
+    let input = File::open("data/day1/input.txt").unwrap();
+
+
+    let total: i32 = BufReader::new(input).lines().map(|line| {
+        line.unwrap().parse::<i32>().unwrap()
+    }).sum();
+
+    print!("Current frequency is {}", total)
 }
